0358700f55f3ee4e711ae2032906fc03f5267ebf
mikleusevicmarin <mikleusevicmarin@gmail.com>
2023-10-16 18:53:39 +0200

    Added loading scene

---
+++ Assets/Scripts/LoaderCallback.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class LoaderCallback : MonoBehaviour
+{
+    private bool isFirstUpdate = true;
+
+    private void Update()
+    {
+        if (isFirstUpdate)
+        {
+            isFirstUpdate = false;
+
+            Loader.LoadingCallback();
+        }
+    }
+}
