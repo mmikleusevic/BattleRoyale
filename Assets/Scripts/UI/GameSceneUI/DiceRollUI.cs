249a9c5def36af9786d9169aef5e25abc4aef5c4
mikleusevicmarin <mikleusevicmarin@gmail.com>
2023-12-14 15:56:32 +0100

    Added code for dice rolling, fixed prefabs

--- Assets/Scripts/UI/GameSceneUI/DiceRollUI.cs
+++ Assets/Scripts/UI/GameSceneUI/DiceRollUI.cs
@@ -1,51 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DiceRollUI : MonoBehaviour
 {
-    [SerializeField] Button rollButton;
-    [SerializeField] GameObject dice1;
-    [SerializeField] GameObject dice2;
-    [SerializeField] GameObject dice3;
+    [SerializeField] Button rollThreeButton;
+    [SerializeField] Button rollSingleButton;
+    [SerializeField] List<GameObject> dice;
+    [SerializeField] Camera threeDiceCamera;
+    [SerializeField] Camera singleDieCamera;
 
-    private float rotationSpeed = 2f;
+    Vector3 threeDiceCameraPosition;
+    Vector3 singleDiceCameraPosition;
+    private Vector3[] positions;
+    private float interactDistance = 0.51f;
+    private float rotationSpeed = 200f;
     private float rotationTime = 3f;
-    private bool isPressed = false;
+    private float factor;
+
     private void Awake()
     {
-        rollButton.onClick.AddListener(() =>
+        rollThreeButton.onClick.AddListener(() =>
         {
-            isPressed = true;
+            StartCoroutine(RotateDice());
             //rollButton.enabled = false;
         });
+
+        rollSingleButton.onClick.AddListener(() =>
+        {
+            StartCoroutine(RotateDie());
+            //rollButton.enabled = false;
+        });
+
+        positions = new Vector3[4];
     }
 
-    private void FixedUpdate()
+    private void Start()
     {
-        if (!isPressed) return;
+        for(int i = 0; i < dice.Count; i++)
+        {
+            Vector3 position = dice[i].transform.position;
 
-        RotateDice();
+            positions[i] = position;
+        }
+
+        threeDiceCameraPosition = threeDiceCamera.transform.position;
+        singleDiceCameraPosition = singleDieCamera.transform.position;
+
+        factor = rotationSpeed * Time.deltaTime;
     }
 
-    private void RotateDice()
+    private IEnumerator RotateDice()
     {
-        dice1.transform.rotation = Random.rotation;
-        dice2.transform.rotation = Random.rotation;
-        dice3.transform.rotation = Random.rotation;
-
-        rotationTime -= Time.deltaTime;
-
-        if (rotationTime > 0)
+        for (int i = 0; i < dice.Count - 1; i++)
         {
-            dice1.transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
-            dice2.transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
-            dice3.transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+            dice[i].transform.rotation = Random.rotation;
         }
-        else
+
+        while (rotationTime > 0)
         {
-            rotationTime = 3f;
-            isPressed = false;
-            Debug.Log("a");
+            rotationTime -= Time.deltaTime;
+
+            for (int i = 0; i < dice.Count - 1; i++)
+            {
+                dice[i].transform.Rotate(new Vector3(Random.Range(0f, 1f) * factor, Random.Range(0f, 1f) * factor, Random.Range(0f, 1f)) * factor);
+            }
+
+            yield return null;
         }
+
+        int result = 0;
+
+        for(int i = 0; i < dice.Count - 1; i++)
+        {
+            Vector3 direction = positions[i] - threeDiceCameraPosition;
+
+            result += CheckSide(direction, threeDiceCameraPosition);
+        }
+
+        Debug.Log(result);
+
+        rotationTime = 3f;
+    }
+
+    private IEnumerator RotateDie()
+    {
+        dice[3].transform.rotation = Random.rotation;
+
+        while (rotationTime > 0)
+        {
+            rotationTime -= Time.deltaTime;
+
+            dice[3].transform.Rotate(new Vector3(Random.Range(0f, 1f) * factor, Random.Range(0f, 1f) * factor, Random.Range(0f, 1f)) * factor);
+
+            yield return null;
+        }
+
+        Vector3 direction = positions[3] - singleDiceCameraPosition;
+
+        int result = CheckSide(direction, singleDiceCameraPosition);
+
+        Debug.Log(result);
+
+        rotationTime = 3f;
+    }
+
+
+    public int CheckSide(Vector3 direction, Vector3 cameraPosition)
+    {        
+        Physics.Raycast(cameraPosition, direction, out RaycastHit raycastHit, interactDistance);
+
+        //TODO face camera when figured which side is hit with raycast
+
+        int.TryParse(raycastHit.collider.name, out int result);        
+
+        return result;
     }
 }
