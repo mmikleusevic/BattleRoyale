826cde933ecff9cd1953de939f2772b519fa0023
mikleusevicmarin <mikleusevicmarin@gmail.com>
2023-12-15 16:22:18 +0100

    Fixed model, did logic for dice rolling

--- Assets/Scripts/UI/GameSceneUI/DiceRollUI.cs
+++ Assets/Scripts/UI/GameSceneUI/DiceRollUI.cs
@@ -15,8 +15,8 @@ public class DiceRollUI : MonoBehaviour
     Vector3 singleDiceCameraPosition;
     private Vector3[] positions;
     private float interactDistance = 0.51f;
-    private float rotationSpeed = 200f;
-    private float rotationTime = 3f;
+    private float rotationSpeed = 150f;
+    private float rotationTime = 2f;
     private float factor;
 
     private void Awake()
@@ -24,13 +24,13 @@ public class DiceRollUI : MonoBehaviour
         rollThreeButton.onClick.AddListener(() =>
         {
             StartCoroutine(RotateDice());
-            //rollButton.enabled = false;
+            //rollThreeButton.gameObject.SetActive(false);
         });
 
         rollSingleButton.onClick.AddListener(() =>
         {
             StartCoroutine(RotateDie());
-            //rollButton.enabled = false;
+            //rollSingleButton.gameObject.SetActive(false);
         });
 
         positions = new Vector3[4];
@@ -38,7 +38,7 @@ public class DiceRollUI : MonoBehaviour
 
     private void Start()
     {
-        for(int i = 0; i < dice.Count; i++)
+        for (int i = 0; i < dice.Count; i++)
         {
             Vector3 position = dice[i].transform.position;
 
@@ -53,35 +53,29 @@ public class DiceRollUI : MonoBehaviour
 
     private IEnumerator RotateDice()
     {
+        int result = 0;
+
         for (int i = 0; i < dice.Count - 1; i++)
         {
             dice[i].transform.rotation = Random.rotation;
-        }
 
-        while (rotationTime > 0)
-        {
-            rotationTime -= Time.deltaTime;
-
-            for (int i = 0; i < dice.Count - 1; i++)
+            while (rotationTime > 0)
             {
+                rotationTime -= Time.deltaTime;
+
                 dice[i].transform.Rotate(new Vector3(Random.Range(0f, 1f) * factor, Random.Range(0f, 1f) * factor, Random.Range(0f, 1f)) * factor);
+
+                yield return null;
             }
 
-            yield return null;
-        }
-
-        int result = 0;
-
-        for(int i = 0; i < dice.Count - 1; i++)
-        {
             Vector3 direction = positions[i] - threeDiceCameraPosition;
 
-            result += CheckSide(direction, threeDiceCameraPosition);
+            result += GetSideAndRotate(direction, threeDiceCameraPosition, dice[i]);
+
+            rotationTime = 2f;
         }
 
         Debug.Log(result);
-
-        rotationTime = 3f;
     }
 
     private IEnumerator RotateDie()
@@ -99,7 +93,7 @@ public class DiceRollUI : MonoBehaviour
 
         Vector3 direction = positions[3] - singleDiceCameraPosition;
 
-        int result = CheckSide(direction, singleDiceCameraPosition);
+        int result = GetSideAndRotate(direction, singleDiceCameraPosition, dice[3]);
 
         Debug.Log(result);
 
@@ -107,14 +101,61 @@ public class DiceRollUI : MonoBehaviour
     }
 
 
-    public int CheckSide(Vector3 direction, Vector3 cameraPosition)
-    {        
+    private int GetSideAndRotate(Vector3 direction, Vector3 cameraPosition, GameObject die)
+    {
         Physics.Raycast(cameraPosition, direction, out RaycastHit raycastHit, interactDistance);
 
-        //TODO face camera when figured which side is hit with raycast
+        int.TryParse(raycastHit.collider.name, out int result);
 
-        int.TryParse(raycastHit.collider.name, out int result);        
+        Vector3 side = GetSide(result);
+
+        StartCoroutine(RotateToward(side, die));
 
         return result;
+    }
+
+    private Vector3 GetSide(int number)
+    {
+        Vector3 side = Vector3.zero;
+
+        switch (number)
+        {
+            case 1:
+                side = Vector3.right;
+                break;
+            case 2:
+                side = Vector3.up;
+                break;
+            case 3:
+                side = Vector3.back;
+                break;
+            case 4:
+                side = Vector3.forward;
+                break;
+            case 5:
+                side = Vector3.down;
+                break;
+            case 6:
+                side = Vector3.left;
+                break;
+        }
+
+        return side;
+    }
+
+    private IEnumerator RotateToward(Vector3 side, GameObject die)
+    {
+        float timer = 1f;
+
+        while (timer > 0)
+        {
+            timer -= Time.deltaTime;
+
+            Quaternion rotation = Quaternion.LookRotation(side);
+
+            die.transform.rotation = Quaternion.Slerp(die.transform.rotation, rotation, 10f * Time.deltaTime);
+
+            yield return null;
+        }
     }
 }
