4260e17bbff49ddf6b61a1ffc59fb84c08337295
mikleusevicmarin <mikleusevicmarin@gmail.com>
2024-03-27 17:58:25 +0100

    commit

--- Assets/Scripts/GameScene/Roll.cs
+++ Assets/Scripts/GameScene/Roll.cs
@@ -2,7 +2,6 @@ using DG.Tweening;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using static UnityEditor.Progress;
 using Random = UnityEngine.Random;
 
 public class Roll : MonoBehaviour
